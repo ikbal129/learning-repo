5d23e3117d30175fb29019c6c3350da97b4d957e
ikbal129 <kurbaniikbal3@gmail.com>
2026-07-14 15:16:06 +0800

    Hapus Folder yg salah

--- Memory Game/acak.js
+++
@@ -1,9 +0,0 @@
-export function shuffle(arr) {
-    let data = [...arr]
-    for (let i = data.length - 1; i > 0; i--) {
-        const newIndex = Math.floor(Math.random() * (i + 1));
-        [data[i], data[newIndex]] = [data[newIndex], data[i]];
-    }
-    
-    return data
-}
